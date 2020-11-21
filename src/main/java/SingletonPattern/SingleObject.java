39af70e22bf8a0147482d15955e0f5f9a940afd0
JustMaoshu <mf20320126@smail.nju.edu.cn>
2020-11-21 10:40:48 +0800

    修改单例模式，增加备注。

--- src/main/java/SingletonPattern/SingleObject.java
+++ src/main/java/SingletonPattern/SingleObject.java
@@ -1,12 +1,18 @@
 package SingletonPattern;
 
+/**
+ * 线程不安全的单例模式，多线程同时进入会造成instance被多次实例化，所以在多线程不能正常工作
+ */
 public class SingleObject {
 
-    private static SingleObject instance = new SingleObject();
+    private static SingleObject instance;
 
     private SingleObject(){}
 
     public static SingleObject getInstance(){
+        if(instance == null){
+            instance = new SingleObject();
+        }
         return instance;
     }
 
